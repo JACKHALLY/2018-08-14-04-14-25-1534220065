9f24dfd61e32cd0e9a9d708407db6c30ee98c390
jackhally <qq1223386926@gmail.com>
2019-05-28 11:58:10 +0800

    lcd

--- main/main.js
+++ main/main.js
@@ -1,4 +1,25 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(s) {
+  digitSymbols = [["._.", "...", "._.", "._.", "...", "._.", "._.", "._.", "._.", "._."],
+            ["|.|", "..|", "._|", "._|", "|_|", "|_.", "|_.", "..|", "|_|", "|_|"],
+            ["|_|", "..|", "|_.", "._|", "..|", "._|", "|_|", "..|", "|_|", "..|"]];
+
+  digits = [];
+  for(i = 0; i < s.length; ++i) {
+    digits.push(parseInt(s[i]));
+  }
+
+  result = "";
+  for(i = 0; i < digitSymbols.length; ++i) {
+    for(j = 0; j < digits.length; ++j) {
+      result += digitSymbols[i][digits[j]];
+      if(j != digits.length - 1) {
+        result += " ";
+      }
+      else {
+        result += "\n";
+      }
+    }
+  }
+
+  return result;
 };
